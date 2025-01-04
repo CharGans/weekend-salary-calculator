bf9de985c6f939aa38aaa77a9578368eafc60169
Charlotte Ganske <cganske04@icloud.com>
2025-01-03 21:00:43 -0600

    added tables

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Hello world!');
+
+function submitForm(event) {
+    console.log('submit form');
+//stop page from refreshing
+    event.preventDefault();
+//make variables with the value of HTML id's
+let firsVal = document.querySelector('#first-input')
+
+
+
+};

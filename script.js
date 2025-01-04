c52cb21508d6da2b617cd33ab593736d3756fef2
Charlotte Ganske <cganske04@icloud.com>
2025-01-03 21:31:49 -0600

    added function to add employee info

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ function submitForm(event) {
 //stop page from refreshing
     event.preventDefault();
 //make variables with the value of HTML id's
-let firsVal = document.querySelector('#first-input')
+let firstVal = document.querySelector('#first-input').value;
+let lastVal = document.querySelector('#last-input').value;
+let idVal = document.querySelector('#id-input').value;
+let titleVal = document.querySelector('#title-input').value;
+let salaryVal = document.querySelector('#salary-input').value;
+//table variable
+let employeeTable = document.querySelector('#employee-info')
+//remove button still needed
 
+employeeTable.innerHTML += `
+    <tr>
+        <td>${firstVal}</td>
+        <td>${lastVal}</td>
+        <td>${idVal}</td>
+        <td>${titleVal}</td>
+        <td>${salaryVal}</td>
+        
 
-
+`
 };
+
+//still need function to remove text in imput fields after submit

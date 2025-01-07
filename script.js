d55f44c676284b520b968354bf96495011036a02
Charlotte Ganske <cganske04@icloud.com>
2025-01-06 21:24:36 -0600

    added input reset function

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ console.log('Hello world!');
 
 function submitForm(event) {
     console.log('submit form');
-//stop page from refreshing
+    //salaryTotal(event);
 
+//stop page from refreshing
     event.preventDefault();
+
 //make variables with the value of HTML id's
   let firstVal = document.querySelector('#first-input').value;
   let lastVal = document.querySelector('#last-input').value;
   let idVal = document.querySelector('#id-input').value;
   let titleVal = document.querySelector('#title-input').value;
   let salaryVal = document.querySelector('#salary-input').value;
-//table variable
+
+//HTML table variable
   let employeeTable = document.querySelector('#employee-data');
-//remove 
+
+//remove tr
   let delButtn = `<button onclick="deleteButton(event)">Remove</button>`;
 
 employeeTable.innerHTML += `
@@ -25,13 +29,34 @@ employeeTable.innerHTML += `
         <td>${salaryVal}</td>
         <td>${delButtn}</td>
    </tr> 
-`;
+`
+// call inputReset function
+inputReset();
 };
 
+//delete button
 function deleteButton(event) {
         let parentRow = event.target.parentElement.parentElement
         console.log('table row removed');
         parentRow.remove();
-    };
+};
+
+
+function inputReset() {
+  document.querySelector('#first-input').value = '';
+  document.querySelector('#last-input').value = '';
+  document.querySelector('#id-input').value = '';
+  document.querySelector('#title-input').value = '';
+  document.querySelector('#salary-input').value = '';
+};
+
+
+let total = 0;
+
+
+function salaryTotal(event) {
+let sal = +document.querySelector('#salary-input').value;
+total += sal
+};
 
 //still need function to remove text in imput fields after submit

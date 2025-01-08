cce8614483307ab7405f46168317b3b68a6c8b9e
Charlotte Ganske <cganske04@icloud.com>
2025-01-07 20:48:34 -0600

    added Salary Total Function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 console.log('Hello world!');
 
+//variable for table
+let employeeTable = document.querySelector('#employee-data');
+
+
+
 function submitForm(event) {
     console.log('submit form');
-    salaryTotal(event);
 
 //stop page from refreshing
     event.preventDefault();
@@ -13,9 +17,6 @@ function submitForm(event) {
   let idVal = document.querySelector('#id-input').value;
   let titleVal = document.querySelector('#title-input').value;
   let salaryVal = document.querySelector('#salary-input').value;
-
-//HTML table variable
-  let employeeTable = document.querySelector('#employee-data');
 
 //remove tr
   let delButtn = `<button onclick="deleteButton(event)">Remove</button>`;
@@ -29,14 +30,19 @@ employeeTable.innerHTML += `
         <td>${salaryVal}</td>
         <td>${delButtn}</td>
    </tr> 
-`
+`;
+
 // call inputReset function
 inputReset();
+
+//adds new salary to total
+let totalSalary = salaryTotal(employeeTable, [4]);
+updateSalFooter(totalSalary);
 };
 
 //delete button
 function deleteButton(event) {
-        let parentRow = event.target.parentElement.parentElement
+        let parentRow = event.target.parentElement.parentElement;
         console.log('table row removed');
         parentRow.remove();
 };
@@ -51,19 +57,45 @@ function inputReset() {
 };
 
 //function to add salaries when added 
-let total = 0;
+function salaryTotal(table, columnIndex) {
+  let sum = 0; 
 
-function salaryTotal(event) {
-let sal = +document.querySelector('#salary-input').value;
-total += sal;
-console.log('salary total', total);
-
-updateSalFooter();
+for (let i = 0; i < table.rows.length; i++) {
+  let cell = table.rows[i].cells[columnIndex];
+  let value = +cell.textContent;
+  sum += value
+}
+console.log('Current Sum', sum);
+return sum;
 };
+
+function updateSalFooter(total) {
+document.querySelector('#sal-total-p').innerHTML = `${total}`;
+};
+
+
+
+
+//let sal = +document.querySelector('#salary-input').value;
+//total += sal;
+//console.log('salary total', total);
+
+//updateSalFooter();
+
 
 //function to change sal total number
-function updateSalFooter() {
-let salFooter = document.querySelector('#sal-total-div');
-salFooter.innerHTML +=`
-<p>${total}</p> `
-};
+//document.querySelector('#sal-total-p').innerhtml = `${total}`;
+
+//let salFooter = document.querySelector('#sal-total-div');
+//salFooter.innerHTML +=`<p>${total}</p>`
+
+
+//function salaryTotal(event) {
+// let sal = +document.querySelector('#salary-input').value;
+// total += sal
+//};
+  
+
+
+
+

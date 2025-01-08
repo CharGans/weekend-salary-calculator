c850500d015e068c078665a42d39d229aec66ca1
Charlotte Ganske <cganske04@icloud.com>
2025-01-08 16:18:00 -0600

    Attempted to fix server bug

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-/* 
 const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use(express.static());
+app.use(express.static('server/public'));
 
 app.listen(port,() => {
     console.log(`listen to port: ${port}`);
 });
-*/
 
-//Server not working? 
-//Error 
-/*
-Error: Cannot find module '/server.js'
-    at Function._resolveFilename (node:internal/modules/cjs/loader:1249:15)
-    at Function._load (node:internal/modules/cjs/loader:1075:27)
-    at TracingChannel.traceSync (node:diagnostics_channel:322:14)
-    at wrapModuleLoad (node:internal/modules/cjs/loader:219:24)
-    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)
-    at node:internal/main/run_main_module:36:49 {
-  code: 'MODULE_NOT_FOUND',
-  requireStack: []
-}
-*/
+//having trouble getting my server to work
+//when i open http://localhost:5001 it displays "Cannot GET /"
+//i think i might be missing somthing 
